b8d95a431695447397778f778ab11b1fcb6e297b
stella2302 <43129929+stella2302@users.noreply.github.com>
2018-12-13 14:20:30 +0100

    Add files via upload

---
+++ 04_03_01_web_prototype/scripts.js
@@ -0,0 +1,68 @@
+//Materialer (Sofia):
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+
+	acc[i].addEventListener("click", function () {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
+
+//Materialer Slut
+
+//Materialer (Irene):
+
+var acc = document.getElementsByClassName("accordion0");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+
+	acc[i].addEventListener("click", function () {
+		/* Toggle between adding and removing the "active" class,
+		to highlight the button that controls the panel */
+		this.classList.toggle("active");
+
+		/* Toggle between hiding and showing the active panel */
+		var panel = this.nextElementSibling;
+		if (panel.style.display === "block") {
+			panel.style.display = "none";
+		} else {
+			panel.style.display = "block";
+		}
+	});
+}
+
+//Materialer Slut
+
+
+
+/* Asbjørn */
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
